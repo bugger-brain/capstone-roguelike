918c521d0fe4a29ae72c9fde13746531a8dd9c58
Paul <pgotcher@dev-10.com>
2022-02-17 15:54:18 -0600

    fetched game obj from REST api

--- server/src/main/java/learn/roguelike/controllers/GameController.java
+++ server/src/main/java/learn/roguelike/controllers/GameController.java
@@ -1,7 +1,29 @@
 package learn.roguelike.controllers;
 
+import learn.roguelike.domain.GameService;
+import learn.roguelike.models.Game;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/roguelike")
+//@ConditionalOnWebApplication
 public class GameController {
 
+    private final GameService service;
+
+    public GameController(GameService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Game> getGames(){
+        return service.findAll();
+    }
 
 
 }
