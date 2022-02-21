5e29fa4c61cc4a8442fa7b98839830bbdd74c019
Sheridan Ash <sheridanash7@gmail.com>
2022-02-21 11:56:21 -0600

    find all js api functions

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,3 +1,21 @@
-function Game(){}
+import { useEffect, useState } from "react";
+import { findAll } from "../services/game-api";
+function Game(){
+
+    const [games, setGames] = useState([]);
+
+
+    useEffect(() => {
+        findAll()
+        .then(json => setGames(json))
+        .catch(console.error)
+    }, []); 
+
+    return (
+        <div>
+            {games.map(g => <div>{g.gameId}</div>)}
+        </div>
+    );
+}
 
 export default Game;
