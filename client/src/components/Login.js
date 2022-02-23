bd015e26590d9ccfc97d0a85943250dcb88d5fec
Paul <pgotcher@dev-10.com>
2022-02-23 13:20:54 -0600

    login fetches player object

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,42 @@
 import { useState, useEffect, useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
+import { findPlayerByUsername } from "../services/player-api";
+
+const blankCandidate = {
+    username: "",
+    password: ""
+};
 
 function Login() {
 
-    const [candidate, setCandidate] = useState({
-        username: "",
-        password: ""
-    });
+    const [candidate, setCandidate] = useState(blankCandidate);
 
     const [hasError, setHasError] = useState(false);
     const authContext = useContext(AuthContext);
 
-    function onSubmit (event) {
+    const onSubmit = event => {
         event.preventDefault();
-        // login(candidate);
-    }
+        login(candidate);
+    };
 
-    function onChange (event)  {
+    const onChange = event => {
         const next = { ...candidate };
         next[event.target.name] = event.target.value;
         setCandidate(next);
-    }
+    };
 
     function login(candidate) {
         findPlayerByUsername(candidate.username)
             .then(player => {
-                localStorage.setItem("player", player);
+                // console.log(player)
+                localStorage.setItem("player", JSON.stringify(player));
             })
             .catch(() => setHasError(true));
+    }
+
+    function whoami() {
+        const p = JSON.parse(localStorage.getItem("player"));
+        console.log(p);
     }
 
     return (
@@ -36,16 +45,19 @@ function Login() {
                 <form onSubmit={onSubmit}>
                     <div className="w-25">
                         <label htmlFor="loginUsername" className="form-label">Username</label>
-                        <input type="text" className="form-control" id="loginUsername"
+                        <input type="text" className="form-control" name="username" id="loginUsername"
                             onChange={onChange} value={candidate.username} required />
                     </div>
                     <div className="w-25 p-3">
                         <label htmlFor="loginPassword" className="form-label">Password</label>
-                        <input type="text" className="form-control" id="loginPassword"
+                        <input type="text" className="form-control" name="password" id="loginPassword"
                             onChange={onChange} value={candidate.password} required />
                     </div>
                     <div>
                         <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                    <div>
+                        <button type="button" onClick={whoami} className="btn btn-primary">whoami</button>
                     </div>
                     {hasError && <div className="alert alert-danger">Bad credentials...</div>}
                 </form>
