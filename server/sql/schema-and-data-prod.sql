aae6629a72371499c7c980bdfd26eaaa465c70f7
Paul <pgotcher@dev-10.com>
2022-02-28 10:01:45 -0600

    added games for steph and shred

--- server/sql/schema-and-data-prod.sql
+++ server/sql/schema-and-data-prod.sql
@@ -104,15 +104,30 @@ insert into player values
     (3, 'shred', '$2a$10$3hxoSGXtjbIoMKcriQmSNuGTZr3X8qyir./R3uzw3jbWn6ZMofKC2');
     
 insert into game values
-	(1, 1, false, 0);
+	(1, 1, false, 0),
+    (2, 2, false, 0),
+    (3, 3, false, 0);
     
 insert into map values
+-- game 1 for pagoto
 	(1, 1, 0, 0), 
     (2, 1, 1, 0),
     (3, 1, 0, 1),
-    (4, 1, 1, 1);
+    (4, 1, 1, 1),
 
+-- game 2 for steph
+	(5, 2, 0, 0), 
+    (6, 2, 1, 0),
+    (7, 2, 0, 1),
+    (8, 2, 1, 1),
+
+-- game 3 for shred
+	(9, 3, 0, 0), 
+    (10, 3, 1, 0),
+    (11, 3, 0, 1),
+    (12, 3, 1, 1);
 insert into tile values
+-- game 1 for pagoto
 	(1, 1, 'grass', 0, 0),
     (2, 1, 'grass', 1, 0),
     (3, 1, 'grass', 0, 1),
@@ -131,10 +146,54 @@ insert into tile values
     (13, 4, 'grass', 0, 0),
     (14, 4, 'grass', 1, 0),
     (15, 4, 'grass', 0, 1),
-    (16, 4, 'grass', 1, 1);
+    (16, 4, 'grass', 1, 1),
+    
+-- game 2 for steph
+	(17, 5, 'grass', 0, 0),
+    (18, 5, 'grass', 1, 0),
+    (19, 5, 'grass', 0, 1),
+    (20, 5, 'grass', 1, 1),
+    
+    (21, 6, 'grass', 0, 0),
+    (22, 6, 'grass', 1, 0),
+    (23, 6, 'grass', 0, 1),
+    (24, 6, 'grass', 1, 1),
+    
+    (25, 7, 'grass', 0, 0),
+    (26, 7, 'grass', 1, 0),
+    (27, 7, 'grass', 0, 1),
+    (28, 7, 'grass', 1, 1),
+    
+    (29, 8, 'grass', 0, 0),
+    (30, 8, 'grass', 1, 0),
+    (31, 8, 'grass', 0, 1),
+    (32, 8, 'grass', 1, 1),
+
+-- game 3 for shred
+	(33, 9, 'grass', 0, 0),
+    (34, 9, 'grass', 1, 0),
+    (35, 9, 'grass', 0, 1),
+    (36, 9, 'grass', 1, 1),
+    
+    (37, 10, 'grass', 0, 0),
+    (38, 10, 'grass', 1, 0),
+    (39, 10, 'grass', 0, 1),
+    (40, 10, 'grass', 1, 1),
+    
+    (41, 11, 'grass', 0, 0),
+    (42, 11, 'grass', 1, 0),
+    (43, 11, 'grass', 0, 1),
+    (44, 11, 'grass', 1, 1),
+    
+    (45, 12, 'grass', 0, 0),
+    (46, 12, 'grass', 1, 0),
+    (47, 12, 'grass', 0, 1),
+    (48, 12, 'grass', 1, 1);
 
 insert into hero values
-	(1, 1, 1, 10, 3, false, false, false, false, 0, 50);
+	(1, 1, 1, 10, 3, false, false, false, false, 0, 50),
+    (2, 2, 17, 10, 3, false, false, false, false, 0, 50),
+    (3, 3, 33, 10, 3, false, false, false, false, 0, 50);
     
 insert into monster values
 	(1, 2, 20);
